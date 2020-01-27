69a51187db527acb8581bd0a2713ee41157f83a5
Shamal Sandeep <ssdh.gdrive@gmail.com>
2020-01-28 00:49:26 +0530

    New receipt print implemented

--- app/src/main/java/app/agrivest/android/IssueReceipt.java
+++ app/src/main/java/app/agrivest/android/IssueReceipt.java
@@ -3,29 +3,48 @@ package app.agrivest.android;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Looper;
+import android.util.Log;
 import android.widget.ProgressBar;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.bxl.config.editor.BXLConfigLoader;
 
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
 import android.os.Handler;
+import android.widget.Toast;
 
 import jpos.JposConst;
 import jpos.JposException;
 import jpos.POSPrinter;
 import jpos.POSPrinterConst;
+import jpos.config.JposEntry;
 
 public class IssueReceipt implements Runnable {
     Thread thrd;
     Context context;
     ProgressDialog progressDialog;
     HashMap<String, String> receiptDetails;
-    BXLConfigLoader bxlConfigLoader;
     Utils utils;
     AlertDialog printReceiptStatus;
+    Handler mainHandler;
+    SharedPreferences userDetails;
+    private RequestQueue mQueue;
 
     IssueReceipt(Context context, ProgressDialog progressDialog, HashMap<String, String> receiptDetails) {
         this.context = context;
@@ -36,98 +55,165 @@ public class IssueReceipt implements Runnable {
         thrd.start();
     }
 
+    private void showMessage(final String type, final String title, final String message) {
+        progressDialog.dismiss();
+        Runnable myRunnable = new Runnable() {
+            @Override
+            public void run() {
+                printReceiptStatus = new AlertDialog.Builder(context).create();
+                printReceiptStatus.setTitle(title);
+                printReceiptStatus.setMessage(message);
+                printReceiptStatus.setIcon(context.getResources().getDrawable(type.equals("success") ? R.drawable.success_icon : R.drawable.failure_icon));
+                printReceiptStatus.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        });
+                printReceiptStatus.show();
+            }
+        };
+        mainHandler.post(myRunnable);
+    }
+
     @Override
     public void run() {
-        Handler mainHandler = new Handler(context.getMainLooper());
+        mainHandler = new Handler(context.getMainLooper());
+
+        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        Set<BluetoothDevice> bondedDeviceSet = bluetoothAdapter.getBondedDevices();
+
+        Iterator<BluetoothDevice> btItr = bondedDeviceSet.iterator();
+
+        boolean paired = false;
+        String MAC = null;
+        while (btItr.hasNext()) {
+            BluetoothDevice bt = btItr.next();
+            if (bt.getName().equals(BXLConfigLoader.PRODUCT_NAME_SPP_R210)) {
+                paired = true;
+                MAC = bt.getAddress();
+            }
+        }
+
+        if (!paired) {
+            showMessage(
+                    "Failure",
+                    "Printer not paired",
+                    "Your device is not paired with the printer. Please go to bluetooth settings and make sure " + BXLConfigLoader.PRODUCT_NAME_SPP_R210 + " exists under paired devices section."
+            );
+            return;
+        }
+
+        BXLConfigLoader bxlConfigLoader = new BXLConfigLoader(context);
         try {
-            bxlConfigLoader = new BXLConfigLoader(context);
             bxlConfigLoader.openFile();
+        } catch (Exception e) {
+            e.printStackTrace();
+            bxlConfigLoader.newFile();
+        }
+
+        try {
+            for (Object entry : bxlConfigLoader.getEntries()) {
+                JposEntry jposEntry = (JposEntry) entry;
+                bxlConfigLoader.removeEntry(jposEntry.getLogicalName());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            showMessage(
+                    "Failure",
+                    "Something went wrong",
+                    "Please contact system administrator"
+            );
+            return;
+        }
+
+        try {
+            bxlConfigLoader.addEntry(BXLConfigLoader.PRODUCT_NAME_SPP_R210,
+                    BXLConfigLoader.DEVICE_CATEGORY_POS_PRINTER,
+                    BXLConfigLoader.PRODUCT_NAME_SPP_R210,
+                    BXLConfigLoader.DEVICE_BUS_BLUETOOTH,
+                    MAC);
             bxlConfigLoader.saveFile();
+        } catch (Exception e) {
+            e.printStackTrace();
+            showMessage(
+                    "Failure",
+                    "Something went wrong",
+                    "Please contact system administrator"
+            );
+            return;
+        }
+
+        try {
             POSPrinter posPrinter = new POSPrinter(context);
-            posPrinter.open("SPP-R210");
+            posPrinter.open(BXLConfigLoader.PRODUCT_NAME_SPP_R210);
             posPrinter.claim(5000);
             posPrinter.setDeviceEnabled(true);
             posPrinter.checkHealth(JposConst.JPOS_CH_INTERNAL);
-            posPrinter.printNormal(POSPrinterConst.PTR_S_RECEIPT, "Agrivest (Private) Limited\n");
-            posPrinter.printNormal(POSPrinterConst.PTR_S_RECEIPT, "--------------------------------\n");
-            posPrinter.printNormal(POSPrinterConst.PTR_S_RECEIPT, "Customer Name " + receiptDetails.get("customer_name") + "\n");
-            posPrinter.printNormal(POSPrinterConst.PTR_S_RECEIPT, "Chassis Number " + receiptDetails.get("chassis_number") + "\n");
-            posPrinter.printNormal(POSPrinterConst.PTR_S_RECEIPT, "Amount LKR " + receiptDetails.get("amount") + "\n");
-            posPrinter.printNormal(POSPrinterConst.PTR_S_RECEIPT, "\n");
-            posPrinter.printNormal(POSPrinterConst.PTR_S_RECEIPT, "\n");
-            posPrinter.printNormal(POSPrinterConst.PTR_S_RECEIPT, "\n");
-            posPrinter.printNormal(POSPrinterConst.PTR_S_RECEIPT, "\n");
-            posPrinter.printNormal(POSPrinterConst.PTR_S_RECEIPT, "\n");
-            posPrinter.printNormal(POSPrinterConst.PTR_S_RECEIPT, "\n");
-            posPrinter.printNormal(POSPrinterConst.PTR_S_RECEIPT, "\n");
-            posPrinter.printNormal(POSPrinterConst.PTR_S_RECEIPT, "\n");
-            posPrinter.printNormal(POSPrinterConst.PTR_S_RECEIPT, "\n");
-            posPrinter.printNormal(POSPrinterConst.PTR_S_RECEIPT, "\n");
+            posPrinter.printNormal(POSPrinterConst.PTR_S_RECEIPT, "Agrivest (Private) Limited\n" +
+                    "Customer Name " + receiptDetails.get("customer_name") + "\n" +
+                    "Chassis Number " + receiptDetails.get("chassis_number") + "\n" +
+                    "Amount LKR " + receiptDetails.get("amount") + "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
             posPrinter.close();
+        } catch (JposException e) {
+            e.printStackTrace();
+            showMessage(
+                    "Failure",
+                    "Printing error",
+                    "Please check the following details\n" +
+                            "1. Printer is turned on\n" +
+                            "2. Printer is within 1m range\n" +
+                            "3. Paper roll not empty\n" +
+                            "4. Paper cover closed"
+            );
+            return;
+        }
 
-            if (utils.isInternetAvailable(context)) {
-                progressDialog.dismiss();
-                Runnable myRunnable = new Runnable() {
-                    @Override
-                    public void run() {
-                        printReceiptStatus = new AlertDialog.Builder(context).create();
-                        printReceiptStatus.setTitle("Receipt issued");
-                        printReceiptStatus.setMessage("Receipt issued successfully.");
-                        printReceiptStatus.setIcon(context.getResources().getDrawable(R.drawable.success_icon));
-                        printReceiptStatus.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
-                                new DialogInterface.OnClickListener() {
-                                    @Override
-                                    public void onClick(DialogInterface dialog, int which) {
-                                        dialog.dismiss();
-                                    }
-                                });
-                        printReceiptStatus.show();
-                    }
-                };
-                mainHandler.post(myRunnable);
-            } else {
-                progressDialog.dismiss();
-                Runnable myRunnable = new Runnable() {
-                    @Override
-                    public void run() {
-                        printReceiptStatus = new AlertDialog.Builder(context).create();
-                        printReceiptStatus.setTitle("Receipt saved to offline receipts");
-                        printReceiptStatus.setMessage("You do not currently have an active internet connection. Therefore this receipt was saved as an offline receipt. You should connect to internet and upload this receipt at your earliest convenince.");
-                        printReceiptStatus.setIcon(context.getResources().getDrawable(R.drawable.success_icon));
-                        printReceiptStatus.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
-                                new DialogInterface.OnClickListener() {
-                                    @Override
-                                    public void onClick(DialogInterface dialog, int which) {
-                                        dialog.dismiss();
-                                    }
-                                });
-                        printReceiptStatus.show();
-                    }
-                };
-                mainHandler.post(myRunnable);
-            }
-        }catch(JposException e) {
-            progressDialog.dismiss();
-            Runnable myRunnable = new Runnable() {
+        userDetails = context.getSharedPreferences("user_details", context.MODE_PRIVATE);
+        if(utils.isInternetAvailable(context)) {
+            mQueue = Volley.newRequestQueue(context);
+            String url = "https://agrivest.app/api/contract/receipt";
+            StringRequest request = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
                 @Override
-                public void run() {
-                    printReceiptStatus = new AlertDialog.Builder(context).create();
-                    printReceiptStatus.setTitle("Printer error");
-                    printReceiptStatus.setMessage("Something wrong with the printer. Please make sure the status light is green and the error light is not on.");
-                    printReceiptStatus.setIcon(context.getResources().getDrawable(R.drawable.failure_icon));
-                    printReceiptStatus.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
-                            new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-                                    dialog.dismiss();
-                                }
-                            });
-                    printReceiptStatus.show();
+                public void onResponse(String response) {
+                    showMessage(
+                            "success",
+                            "[ONLINE] Receipt issued",
+                            "Receipt number is " + response
+                    );
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    error.printStackTrace();
+                }
+            }) {
+                @Override
+                protected Map<String, String> getParams() {
+                    Map<String, String> params = new HashMap<String, String>();
+                    params.put("cid", receiptDetails.get("id"));
+                    params.put("user_id", userDetails.getString("id", ""));
+                    params.put("amount", receiptDetails.get("amount"));
+                    return params;
+                }
+
+                @Override
+                public Map<String, String> getHeaders() throws AuthFailureError {
+                    Map<String, String> params = new HashMap<String, String>();
+                    params.put("Authorization", "Bearer " + userDetails.getString("token", ""));
+                    params.put("Content-Type", "application/x-www-form-urlencoded");
+                    return params;
                 }
             };
-            mainHandler.post(myRunnable);
-        }catch (Exception e) {
-            e.printStackTrace();
+
+            mQueue.add(request);
+        } else {
+            showMessage(
+                    "success",
+                    "[OFFLINE] Receipt issued",
+                    "Please upload the receipt as soon as you connect to the internet"
+            );
         }
     }
 }
