65f371a5ce6aa2688c70c3d58731414cb7aa1d37
Shamal Sandeep <ssdh.gdrive@gmail.com>
2020-03-26 14:05:26 +0530

    Changed API to production

--- app/src/main/java/app/agrivest/android/api/API.java
+++ app/src/main/java/app/agrivest/android/api/API.java
@@ -1,7 +1,7 @@
 package app.agrivest.android.api;
 
 public class API {
-    private final String APILINK = "https://www.agrivest.app/api-dev";
+    private final String APILINK = "https://www.agrivest.app/api";
 
     public String getApiLink() {
         return this.APILINK;
